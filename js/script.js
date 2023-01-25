f9c1c88fb2976ee14bce5f81165c5cdeb5edc135
Zelimkhan <delfinbel@gmail.com>
2023-01-25 13:08:41 +0100

    startet prosjektet

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = '5';
+
+const bool = false;
+
+//console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name:"John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
